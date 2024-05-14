6cf15b45fe9722a47614521d40f20f6cd7376087
unknown <Juan>
2024-05-13 23:07:02 -0500

    subir

---
+++ Grid/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace Grid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNumeroClicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var numero = (string)button.CommandParameter;
+            numeroLabel.Text += numero;
+        }
+
+        private void OnDialClicked(object sender, EventArgs e)
+        {
+            numeroLabel.Text = "";
+        }
+    }
+}
